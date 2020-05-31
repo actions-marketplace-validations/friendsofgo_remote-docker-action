55ac9e0facc19e79599f9d36cafa22621ca2ba83
Joan López de la Franca Beltran <joanjan14@gmail.com>
2020-05-31 18:47:58 +0200

    Add deployment steps

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,10 +14,15 @@ printf '%s' "$INPUT_CACERT" > $CA_CERT_FILE
 printf '%s' "$INPUT_SERVERCERT" > $SERVER_CERT_FILE
 printf '%s' "$INPUT_SERVERKEY" > $SERVER_KEY_FILE
 
+# Configuration used for Docker remote usage
+REMOTE="-H $INPUT_HOSTADDRESS --tls --tlscacert=$CA_CERT_FILE --tlscert=$SERVER_CERT_FILE --tlskey=$SERVER_KEY_FILE"
+
 # Log into Docker registry
 if ! [ -z "$REGISTRY_PASS" ] && ! [ -z "$REGISTRY_USER" ] ; then
-  echo $REGISTRY_PASS | docker -H $INPUT_HOSTADDRESS --tls --tlscacert=$CA_CERT_FILE --tlscert=$SERVER_CERT_FILE --tlskey=$SERVER_KEY_FILE login https://docker.pkg.github.com -u $REGISTRY_USER --password-stdin
+  echo $REGISTRY_PASS | docker $REMOTE login https://docker.pkg.github.com -u $REGISTRY_USER --password-stdin
 fi
 
-# Pulls alpine:latest Docker image (remotely)
-docker-compose -H $INPUT_HOSTADDRESS --tls --tlscacert=$CA_CERT_FILE --tlscert=$SERVER_CERT_FILE --tlskey=$SERVER_KEY_FILE pull
+# Deployment (pull, down, up)
+docker-compose $REMOTE pull
+docker-compose $REMOTE down
+docker-compose $REMOTE up -d
