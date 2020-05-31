89923d9f49bd3c680aa95874f348a73e6680c8e3
Joan López de la Franca Beltran <joanjan14@gmail.com>
2020-05-31 18:06:03 +0200

    Refactor secrets management

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,16 @@
 #!/bin/sh -l
 
+# Managing secrets
+SECRETS_DIR="$HOME/certs"
+
+CA_CERT_FILE="$SECRETS_DIR/ca.pem"
+SERVER_CERT_FILE="$SECRETS_DIR/server.pem"
+SERVER_KEY_FILE="$SECRETS_DIR/server.key"
+
+# Registering secrets from env
+printf '%s' "$INPUT_CACERT" > $CA_CERT_FILE
+printf '%s' "$INPUT_SERVERCERT" > $SERVER_CERT_FILE
+printf '%s' "$INPUT_SERVERKEY" > $SERVER_KEY_FILE
+
 # Pulls alpine:latest Docker image (remotely)
-docker -H $INPUT_HOSTADDRESS --tls --tlscacert=$INPUT_CACERT --tlscert=$INPUT_SERVERCERT --tlskey=$INPUT_SERVERKEY pull alpine:latest
+docker -H $INPUT_HOSTADDRESS --tls --tlscacert=$CA_CERT_FILE --tlscert=$SERVER_CERT_FILE --tlskey=$SERVER_KEY_FILE pull alpine:latest
