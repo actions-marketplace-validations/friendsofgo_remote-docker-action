e47da50ddca1a2265e11e1de629cb96a651a82f8
Joan López de la Franca Beltran <joanjan14@gmail.com>
2020-05-31 14:11:33 +0200

    Update action to do a remote image pull

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:3.10
+FROM docker:19.03.10
 LABEL MAINTAINER = 'Friends of Go (it@friendsofgo.tech)'
 WORKDIR /github/workspace
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-echo "Hello $1, we gonna list your files..."
-echo "$(ls -1 | tr '\n' ' ')"
+# Pulls alpine:latest Docker image (remotely)
+docker -H $INPUT_HOST_ADDRESS --tls --tlscacert=$INPUT_CA_CERT --tlscert=$INPUT_SERVER_CERT --tlskey=$INPUT_SERVER_KEY pull alpine:latest
