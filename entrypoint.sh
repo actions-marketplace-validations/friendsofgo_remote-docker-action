15b60f915909485d7e5d2d29b63711109118beeb
Joan López de la Franca Beltran <joanjan14@gmail.com>
2020-05-31 14:18:56 +0200

    Minor entrypoint fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 # Pulls alpine:latest Docker image (remotely)
-docker -H $INPUT_HOST_ADDRESS --tls --tlscacert=$INPUT_CA_CERT --tlscert=$INPUT_SERVER_CERT --tlskey=$INPUT_SERVER_KEY pull alpine:latest
+docker -H $INPUT_HOSTADDRESS --tls --tlscacert=$INPUT_CACERT --tlscert=$INPUT_SERVERCERT --tlskey=$INPUT_SERVERKEY pull alpine:latest
